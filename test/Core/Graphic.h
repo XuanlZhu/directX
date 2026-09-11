e964c684fe515cab87a487e0bf34aea340ca316c
Sparking+ <63219026+XuanlZhu@users.noreply.github.com>
2026-09-11 14:30:56 +0800

    1

--- test/Core/Graphic.h
+++ test/Core/Graphic.h
@@ -37,6 +37,12 @@ struct MatrixBuffer
     DirectX::XMMATRIX view;
     DirectX::XMMATRIX projection;
 };
+struct Vertex3obj
+{
+    DirectX::XMFLOAT3 position;
+    DirectX::XMFLOAT3 normal;
+    DirectX::XMFLOAT2 texCoord;
+};
 
 class Graphic
 {
