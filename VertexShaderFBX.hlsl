b1af371874d2b497b53db2131fe86d0736d757e5
Sparking+ <63219026+XuanlZhu@users.noreply.github.com>
2026-09-20 15:37:30 +0800

    1

--- VertexShaderFBX.hlsl
+++ VertexShaderFBX.hlsl
@@ -17,6 +17,8 @@ struct VSOutput
     float4 position : SV_POSITION;
     float3 normal   : NORMAL;
     float2 texCoord : TEXCOORD0;
+	
+	float3 worldPosition : TEXCOORD1;// 世界空间位置，给光照计算使用
 };
 float3x3 Inverse3x3(float3x3 m)
 {
@@ -66,6 +68,8 @@ VSOutput main(VSInput input)
 	
     // 传递 UV
     output.texCoord = input.texCoord;
+    //
+    output.worldPosition = input.position;
 
     return output;
 }
